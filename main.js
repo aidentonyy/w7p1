246f84bcd28ad8451d9080d9b83fed371da1ed22
aidentonyy <at1989@york.ac.uk>
2025-03-28 14:51:45 +0000

    asd

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 
-
-function Themes(bg, textCol, font){
+class Themes {
+    constructor(bg, textCol, font) {
         this.bg = bg;
         this.textCol = textCol;
         this.font = font;
-        this.display = function(){
-            return this.bg + "/" + this.textCol + "/" + this.font
-        }
+        this.display = function () {
+            return this.bg + "/" + this.textCol + "/" + this.font;
+        };
     }
+}
 
     let theme1 = new Themes("#000000", "#ffffff", "open-sans");
     let theme2 = new Themes("#ffffff", "#000000", "Geneva");
@@ -16,3 +17,20 @@ function Themes(bg, textCol, font){
     console.log(theme1.display());
     console.log(theme2.display());
     console.log(theme3.display());
+
+
+    // radio buttons
+
+    function addClick(event){
+        console.log("button Clicked")
+    }
+
+    const black = document.getElementById("black").value;
+    const white = document.getElementById("white").value;
+    const hack = document.getElementById("hack").value;
+
+    black.addEventListener("click", addClick());
+    white.addEventListener("click", addClick());
+    hack.addEventListener("click", addClick());
+
+   
